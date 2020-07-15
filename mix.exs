b0ceffef026389eb0f27394e466ed80f645b53da
nyakioK <nyakiokagure@gmail.com>
2020-07-15 09:32:57 +0300

    Creating a project with mix

--- mix.exs
+++ mix.exs
@@ -5,6 +5,9 @@ defmodule Issues.MixProject do
     [
       app: :issues,
       version: "0.1.0",
+      name: "Issues",
+      source_url: "",
+      escript: escript_config(),
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,6 +26,16 @@ defmodule Issues.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      { :httpoison, "~> 1.7" },
+      { :poison, "~> 3.1" },
+      {:ex_doc, "~> 0.21"}
+    ]
+  end
+
+  # Add the location of the main class.
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
